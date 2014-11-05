a725f3243e964973fff71126566ca2da9939504c
Ben Johnson <ben@bjmfactory.com>
2014-11-05 11:08:08 -0600

    Feature: Edit create method for boxes controller

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -6,8 +6,23 @@ class BoxesController < ApplicationController
   end
 
   def create
-    render plain: params[:box].inspect
+    @box = Box.new(box_params)
+
+    @box.save
+    redirect_to @box
+  end
+
   def show
     @box = Box.find(params[:id])
   end
+
+  private
+    def box_params
+      params.require(:box).permit(:title1,
+                                  :url1,
+                                  :title2,
+                                  :url2,
+                                  :title3,
+                                  :url3)
+    end
 end
